4232462f6d8bb1ef95b079da31c12849ada34643
oscardelalanza <oscardelalanza@gmail.com>
2022-01-09 01:07:36 -0800

    rendering products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,8 +3,13 @@ import { ProductProps } from '../interfaces/Products'
 
 class Product extends Component<ProductProps, {}> {
   render() {
-   return (
-     <p>product</p>
+    const { product } = this.props
+    return (
+     <div>
+       <img src={product.img} alt={product.name} />
+       <h3>{product.name}</h3>
+       <p>{product.price}</p>
+     </div>
    )
   }
 }
