1c28dfc710b1418d4dd01612b595325f5707e690
oscardelalanza <oscardelalanza@gmail.com>
2022-01-09 00:41:55 -0800

    adding products

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import './App.css';
 import { Component } from 'react'
 
 class App extends Component {
+  state = {
+    products: [
+      { name: 'Tomato', price: 1500, img: '/products/tomato.jpg' },
+      { name: 'Beans', price: 2500, img: '/products/beans.jpg' },
+      { name: 'Lettuce', price: 500, img: '/products/lettuce.jpg' }
+    ]
+  }
+
   render() {
     return (
       <div>
