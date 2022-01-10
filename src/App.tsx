5563140a39e157abcc366eb3776073120007dce0
oscardelalanza <oscardelalanza@gmail.com>
2022-01-09 17:36:07 -0800

    adding layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import Products from './components/Products'
+import Layout from './components/Layout'
 
 class App extends Component {
   state = {
@@ -13,10 +14,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Products
-          addToCart={() => console.log('empty')}
-          products={this.state.products}
-        />
+        <Layout>
+          <Products
+            addToCart={() => console.log('empty')}
+            products={this.state.products}
+          />
+        </Layout>
       </div>
     )
   }
